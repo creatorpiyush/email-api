a6be365d49cfbbe71b9d0154a069104d4a2fdd5c
creatorpiyush <piyushanand.1221@gmail.com>
2021-11-08 00:10:44 +0530

    basic setup + post data to nodemailer

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5555;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  const { name, email, subject, message } = req.body;
+  //   console.log(message);
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    ignoreTLS: false,
+    auth: {
+      user: process.env.Nodemailer_USER,
+      pass: process.env.Nodemailer_PASS,
+    },
+  });
+
+  const mailData = {
+    to: `${name} <${email}>`,
+    from: process.env.Nodemailer_USER,
+    subject: `${subject}`,
+    html: `${message}`,
+  };
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) return console.log(err);
+
+    return res.send("Email sent");
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port http://localhost:${port}`);
+});
